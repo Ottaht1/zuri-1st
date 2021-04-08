bb6c86db2a706fc3f00bc9a15fe61b7ac67bb72c
theresa <ottahtheresa@gmail.com>
2021-04-08 17:30:58 +0100

    chore

---
+++ main.js
@@ -0,0 +1,15 @@
+// Selection of HTML objects
+const burger = document.querySelector('.burger i');
+const nav = document.querySelector('.nav-links');
+
+// Defining a function
+function toggleNav() {
+    burger.classList.toggle('fa-bars');
+    burger.classList.toggle('fa-times');
+    nav.classList.toggle('nav-active');
+}
+
+// Calling the function after click event occurs
+burger.addEventListener('click', function() {
+    toggleNav();
+});
